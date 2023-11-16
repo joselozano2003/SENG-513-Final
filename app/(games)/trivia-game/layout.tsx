73e1a685f004117af41b0d03022e4a9b3a00d6e6
Colton Gowans <100043529+ColtG5@users.noreply.github.com>
2023-11-16 16:44:57 -0700

    Fixing issue with layout.tsx not allowing deployment

--- app/(games)/trivia-game/layout.tsx
+++ app/(games)/trivia-game/layout.tsx
@@ -1,1 +1,11 @@
 // any layouts used between all pages of the trivia game
+
+import React from "react";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
