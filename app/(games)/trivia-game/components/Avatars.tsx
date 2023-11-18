91cf3adff4476891c922b955eb6f91ac2fb9afa6
ColtG5 <colton.gowans@ucalgary.ca>
2023-11-17 18:15:07 -0700

    mock avatars!

--- app/(games)/trivia-game/components/Avatars.tsx
+++ app/(games)/trivia-game/components/Avatars.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 interface AvatarProps {
     player: string;
+    imgSrc: string;
 }
 
-function Avatar({ player }: AvatarProps) {
+function Avatar({ player, imgSrc }: AvatarProps) {
     return (
         <div className="flex flex-col items-center">
-            <div className="w-16 h-16 bg-gray-300"></div>
+            <div className="w-16 h-16 relative">
+                <Image src={imgSrc} alt={player} layout="fill" />
+            </div>
             <p className="mt-2 text-white">{player}</p>
         </div>
     );
@@ -15,13 +19,22 @@ function Avatar({ player }: AvatarProps) {
 
 export default function Avatars() {
     // could probably rework the player names to be whatever nickname they join the game with
-    const players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"];
+    const players = [
+        { name: "Player 1", img: "/player-1.png" },
+        { name: "Player 2", img: "/player-2.png" },
+        { name: "Player 3", img: "/player-3.png" },
+        { name: "Player 4", img: "/player-4.png" },
+        { name: "Player 5", img: "/player-5.png" },
+        { name: "Player 6", img: "/player-6.png" },
+        { name: "Player 7", img: "/player-7.png" },
+        { name: "Player 8", img: "/player-8.png" },
+    ];
 
     return (
         <div className="flex justify-center items-center">
-            <div className="grid grid-cols-4 gap-x-36 gap-y-24 w-fit p-20 bg-gray-100 bg-opacity-5 rounded-lg">
+            <div className="grid grid-cols-4 w-fit p-16 bg-gray-100 bg-opacity-5 rounded-lg" style={{ gap: "3rem 5rem" }}>
                 {players.map((player, index) => (
-                    <Avatar key={index} player={player} />
+                    <Avatar key={index} player={player.name} imgSrc={player.img} />
                 ))}
             </div>
         </div>
--- app/(games)/trivia-game/components/JoinStuff.tsx
+++ app/(games)/trivia-game/components/JoinStuff.tsx
@@ -7,7 +7,7 @@ export default function JoinStuff() {
             <Link href="/join-lobby">
                 <p className="text-blue-500 underline mr-10 hover:text-blue-700 transition-colors duration-300">Join the lobby here</p>
             </Link>
-            <div className="border p-2 rounded text-white bg-blue-500 font-bold">Join code: 4398</div>
+            <div className="p-2 rounded text-white bg-blue-500 font-bold">Join code: 4398</div>
         </div>
     );
 }
