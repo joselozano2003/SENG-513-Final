f006e859074bf33ba2cc33ff13a6192ba783b6e1
akaforce <gab.rodriguez11@outlook.com>
2023-11-18 14:58:35 -0700

    tried to migrate the game from react to next, but no idea why it's not working

---
+++ app/(games)/memory-game/[id]/game/components/Card.jsx
@@ -0,0 +1,21 @@
+const Card = ({ image, selected, onClick}) => {
+
+
+    return (
+      <div className="card">
+        <div className={selected && 'selected'}>
+          <img alt="" src={image} className="card-face" />
+  
+          <img
+            alt=""
+            className="card-back"
+            src={'/assets/fireship.png'}
+            onClick={onClick}
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Card;
+  
