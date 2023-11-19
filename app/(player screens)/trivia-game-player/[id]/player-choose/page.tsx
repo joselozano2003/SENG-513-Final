adca2ee19437b418cca085b41d90c839b463de74
Richip4 <90368075+Richip4@users.noreply.github.com>
2023-11-18 22:07:35 -0700

    added waiting page, added common background layout

--- app/(player screens)/trivia-game-player/[id]/player-choose/page.tsx
+++ app/(player screens)/trivia-game-player/[id]/player-choose/page.tsx
@@ -10,27 +10,19 @@ import GameTitle from '@/app/(games)/trivia-game/components/GameTitle';
 
 const MobilePlayerPage = () => {
 
-  const bgStyling = {
-    backgroundImage: `url(${blackBrickBackground.src})`,
-    backgroundSize: "100% 100%",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    padding: "7vh 5vw",
-  };
-
     return (
-      <div className={`h-screen bg-cover`} style={bgStyling}>
+      <div>
         <div className="mb-7">
         <GameTitle title="Trivia Game" fontSize="text-6xl" />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <NeonButton href="trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="blue" padding="py-4 px-2 mb-8">
+          <NeonButton href="/trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="blue" padding="py-4 px-2 mb-8">
             Answer 1
           </NeonButton>
-          <NeonButton href="trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="red" padding="py-4 px-2 mb-8">
+          <NeonButton href="/trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="red" padding="py-4 px-2 mb-8">
             Answer 2
           </NeonButton>
-          <NeonButton href="trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="green" padding="py-4 px-2 mb-8">
+          <NeonButton href="/trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="green" padding="py-4 px-2 mb-8">
             Answer 3
           </NeonButton>
           <NeonButton href="/trivia-game-player/55555/player-wait" textSize="text-lg" borderColor="yellow" padding="py-4 px-2 mb-8">
