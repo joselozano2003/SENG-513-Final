ec6f14a7c46a0f5edcd9ac65925ffccdfbe2511e
Colton Gowans <100043529+ColtG5@users.noreply.github.com>
2023-11-20 10:06:26 -0700

    intro screen should fit better now

--- app/(games)/trivia-game/[id]/intro/page.tsx
+++ app/(games)/trivia-game/[id]/intro/page.tsx
@@ -4,14 +4,14 @@ import CoolButton from "../../components/CoolButton";
 export default function page() {
     return (
         <div
-            className="absolute top-0 left-0 flex flex-col justify-end items-center pb-10"
+            className="h-screen w-screen flex flex-col justify-end items-center pb-10"
             style={{
                 backgroundImage: `url("/trivia-intro.png")`,
-                backgroundPosition: "center",
-                backgroundSize: "cover",
+                // backgroundPosition: "center",
+                // backgroundSize: "contain",
+                backgroundSize: "100% 100%",
                 backgroundRepeat: "no-repeat",
-                height: "100vh",
-                width: "100vw",
+                margin: "-4vh -3vw",
             }}
         >
             <div className="flex justify-center ml-80 animate-bounce">
