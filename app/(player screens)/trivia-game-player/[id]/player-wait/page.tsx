6d9cb5e03479bcfa9621d4f68f28a02b51219fee
Richip4 <richispatel04@gmail.com>
2023-11-25 17:39:14 -0700

    Update page.tsx

--- app/(player screens)/trivia-game-player/[id]/player-wait/page.tsx
+++ app/(player screens)/trivia-game-player/[id]/player-wait/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Link from "next/link";
 
-// assets
-import blackBrickBackground from "public/black-brick-wall-textured-background.jpg";
-
 
 const WaitingPage = () => {
 
