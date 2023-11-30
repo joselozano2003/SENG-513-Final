cac8fbdc45997bfaf5507401b9c574761bce1c82
ColtG5 <colton.gowans@ucalgary.ca>
2023-11-30 01:32:03 -0700

    nicer game cards (i think)

--- app/(games)/prompt-game/components/GridCard.tsx
+++ app/(games)/prompt-game/components/GridCard.tsx
@@ -13,20 +13,16 @@ const GridCard = ({ isPrompt, playerName, answer, avatar, points }: GridCardProp
     const hasPlayer = playerName !== "";
 
     return (
-        <div
-            className={`${isPrompt ? "bg-blue-500 text-xl font-semibold text-white" : "bg-white"} p-4 border-4 border-black rounded-md relative ${
-                !hasPlayer && !isPrompt ? "bg-transparent border-none" : ""
-            }`}
-        >
-            {hasPlayer && (
-                <div className={`flex justify-center items-center ${isPrompt ? "" : "h-2/5"}`}>
-                    <div className="flex flex-col items-center">
-                        {avatar && <img src={avatar} alt="Player Avatar" className="w-12 h-12 rounded-full" />}
-                        {playerName && <small>{playerName}</small>}
-                    </div>
-                    {points !== undefined && !isPrompt && <small className="ml-5">Points: {points}</small>}
+        <div className={`${isPrompt ? "bg-blue-500 text-xl font-semibold text-white border-blue-900" : "bg-white border-black"} p-4 border-4 rounded-md relative}
+        ${(hasPlayer || isPrompt) ? "" : "invisible"}`}>
+            <div className={`flex justify-center items-center ${isPrompt ? "" : "h-2/5"}`}>
+                <div className="flex flex-col items-center">
+                    {avatar && <img src={avatar} alt="Player Avatar" className="w-12 h-12 rounded-full" />}
+                    {playerName && <small>{playerName}</small>}
                 </div>
-            )}
+                {points !== undefined && !isPrompt && <small className="ml-5">Points: {points}</small>}
+            </div>
+
             {/* <div className={`${hasPlayer ? "" : "hidden"}`}>
                 <div className={`flex justify-center items-center ${isPrompt ? "" : "h-2/5"}`}>
                     <div className="flex flex-col items-center">
