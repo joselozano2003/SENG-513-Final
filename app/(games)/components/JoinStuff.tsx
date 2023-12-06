a492ccc58052d779ba232559f04843b59d1a7b0c
ColtG5 <colton.gowans@ucalgary.ca>
2023-12-06 00:42:49 -0700

    brought back the join lobby link from the grave

--- app/(games)/components/JoinStuff.tsx
+++ app/(games)/components/JoinStuff.tsx
@@ -4,9 +4,12 @@ interface JoinStuffProps {
 
 export default function JoinStuff({ id }: JoinStuffProps) {
     const joinCode = id;
+    const joinLink = "http://localhost:3000/trivia-game-player";
 
     return (
-        <div className="flex flex-row justify-center items-center w-fit">
+        <div className="flex flex-col justify-center items-center w-fit">
+            <div className="font-bold">Join the game at link:</div>
+            <div className="text-blue-500 underline text-xl mb-4 mt-2">{joinLink}</div>
             <div className="p-3 text-white bg-blue-500 bg-opacity-50 font-bold rounded-full w-fit">Join code: {joinCode}</div>
         </div>
     );
