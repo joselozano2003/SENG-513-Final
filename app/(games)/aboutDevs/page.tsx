efe544e9d2cb1266d7a8748dc1c8a8900bca9959
anmol-magar-rana <148839281+anmol-magar-rana@users.noreply.github.com>
2023-12-06 15:19:44 -0700

    about devs added

---
+++ app/(games)/aboutDevs/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from 'react';
+import styles from './style.module.css';
+
+export default function page() {
+
+    return (
+        <div className={styles.all}>
+            <div className={styles.body}>
+                <div className={styles.container}>
+                <div className={styles.contactBox}>
+                    <h2><u>Reach us</u></h2>
+                    <p>Jose Camilo Lozano Cetina</p>
+                    <p>Gabriel Rodriguez</p>
+                    <p>Richi Patel </p>
+                    <p>Colton Gowans</p>
+                    <p>Jiayi Teh</p>
+                    <p>Anmol Rana</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+        
+            
+}
