e1c4b4923f3dac84b8bdf2e93a43ddc8d931fbd7
ColtG5 <colton.gowans@ucalgary.ca>
2023-12-05 20:57:44 -0700

    wavy prompt game title

--- app/(games)/prompt-game/components/GameTitle.tsx
+++ app/(games)/prompt-game/components/GameTitle.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import styles from "./styles.module.css"
 
 interface GameTitleProps {
     title: string;
 }
 export default function GameTitle({ title }: GameTitleProps) {
     return (
-        <div className="flex">
-
-            <div className="bg-white border-2 border-black p-4 text-center text-6xl md:text-4xl font-bold text-black rounded-xl self-center">{title}</div>
+        <div className="flex justify-center bg-white border-2 border-black p-4 text-center text-6xl md:text-4xl font-bold text-black rounded-xl self-center">
+            {title.split('').map((char, i) => (
+                <span 
+                    key={i} 
+                    className={styles.animateSnake}
+                    style={{ animationDelay: `${i * 0.2}s` }}
+                >
+                    {char}
+                </span>
+            ))}
         </div>
     );
 }
