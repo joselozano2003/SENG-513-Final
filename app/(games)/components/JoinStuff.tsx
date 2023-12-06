f875aaf9b521842fa1227ef9533db1449e13a5c6
ColtG5 <colton.gowans@ucalgary.ca>
2023-12-06 13:56:26 -0700

    slight tweaks

--- app/(games)/components/JoinStuff.tsx
+++ app/(games)/components/JoinStuff.tsx
@@ -11,7 +11,7 @@ export default function JoinStuff({ id }: JoinStuffProps) {
             <div className="font-bold" style={{ textShadow: "1px 1px black" }}>
                 Join the game at link:
             </div>
-            <div className="text-blue-700 underline mb-4 mt-2">{joinLink}</div>
+            <div className="text-blue-600 underline mb-6 mt-2">{joinLink}</div>
             <div className="p-3 text-white bg-blue-500 bg-opacity-70 font-bold rounded-full w-fit" style={{ textShadow: "1px 1px black" }}>
                 Join code: {joinCode}
             </div>
