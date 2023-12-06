133840803dd72868b6f7d77f4985568147e1d78b
ColtG5 <colton.gowans@ucalgary.ca>
2023-12-06 15:24:18 -0700

    Update CoolButton.tsx

--- app/(games)/prompt-game/components/CoolButton.tsx
+++ app/(games)/prompt-game/components/CoolButton.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import Link from "next/link";
+import React from "react";
+import toast from 'react-hot-toast';
+import styles from "./styles.module.css";
 
 interface CoolButtonProps {
     href: string;
@@ -12,16 +15,22 @@ interface CoolButtonProps {
 }
 
 const CoolButton: React.FC<CoolButtonProps> = ({ href, textSize, padding, hoverScale, color, children }) => {
-    const colorClasses = color === "green" ? "bg-green-600 hover:bg-green-700" : "bg-blue-700 hover:bg-blue-700";
+    // const colorClasses = color === "green" ? "bg-green-600 hover:bg-green-700" : "bg-blue-700 hover:bg-blue-700";
+    const colorClasses = color === "green" ? "bg-green-600" : "bg-blue-700";
     const textShadowColor = color === "green" ? "darkgreen" : "blue";
+    const animColorClasses = color === "green" ? "#00960d" : "#2432ff";
 
     return (
         <Link href={href}>
-            <button
+            {/* <button
                 className={`${colorClasses} text-white ${textSize} font-bold ${
                     padding || "py-6 px-8"
                 } rounded-md border-2 border-black transition duration-300 ease-in-out transform ${hoverScale || "hover:scale-110"}`}
-                style={{ textShadow: `2px 2px 10px ${textShadowColor}` }}
+                style={{ textShadow: `2px 2px 10px ${textShadowColor} ${styles.CoolButton}` }}
+            > */}
+            <button
+                className={`${colorClasses} text-white ${textSize} font-bold ${padding || "py-6 px-8"} rounded-md border-2 border-black ${styles.slidingButton}`}
+                style={{ textShadow: `2px 2px 10px ${textShadowColor}`, ['--button-color']: animColorClasses  } as React.CSSProperties}
             >
                 {children}
             </button>
